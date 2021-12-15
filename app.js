a195b002d5b4501ceda2070b08b61215f1332c54
Thai <thainax101@gmail.com>
2021-12-15 17:31:33 +0700

    update

---
+++ app.js
@@ -0,0 +1,14 @@
+const song = document.getElementById("song");
+const playBtn = document.querySelector(".play-inner");
+
+let isPlaying = true;
+playBtn.addEventListener("click",playPause);
+function playPause(){
+    if(isPlaying){
+        song.play();
+        isPlaying = false;
+    }else{
+        song.pause();
+        isPlaying = true;
+    }
+}
